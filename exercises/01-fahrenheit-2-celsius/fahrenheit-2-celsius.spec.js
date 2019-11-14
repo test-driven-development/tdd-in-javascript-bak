220fe09ade2e59aec99bfbb90f4cf0182a6d68a1
mzebib <mzebib@ford.com>
2019-11-14 11:55:25 -0500

    purge redundant test

--- exercises/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
+++ exercises/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
@@ -3,7 +3,7 @@ const conversionConst = 5 / 9;
 const fahrenheit2Celsius = (fahrenheitValue) =>
    (fahrenheitValue - 32) * (conversionConst);
 
-describe.only('convert fahrenheit to celsius', () => {
+describe('convert fahrenheit to celsius', () => {
   it('32 should return 0', () => {
     fahrenheit2Celsius(32).should.equal(0);
   });
@@ -11,6 +11,4 @@ describe.only('convert fahrenheit to celsius', () => {
   it('212 should return 100', () => {
     fahrenheit2Celsius(212).should.equal(100);
   });
-
-  it('-27 should return -32.77');
 });
