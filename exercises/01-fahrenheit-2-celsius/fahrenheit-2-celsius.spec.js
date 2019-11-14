dd5d5420ae2b6fde723416f5b46ffb0662a79a54
mzebib <mzebib@ford.com>
2019-11-14 11:37:41 -0500

    implement fahrenheit to celsius function

--- exercises/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
+++ exercises/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
@@ -1,12 +1,15 @@
-function fahrenheit2Celsius() {
-  return 0;
+function fahrenheit2Celsius(x) {
+  return (x - 32) * (5 / 9);
 }
 
-describe('convert fahrenheit to celsius', () => {
+describe.only('convert fahrenheit to celsius', () => {
   it('32 should return 0', () => {
     fahrenheit2Celsius(32).should.equal(0);
   });
 
-  it('212 should return 100');
+  it('212 should return 100', () => {
+    fahrenheit2Celsius(212).should.equal(100);
+
+  });
   it('-27 should return -32.77');
 });
