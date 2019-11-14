fef263770bd0aa0dde9f84ada25775e12f7a8ee3
mzebib <mzebib@ford.com>
2019-11-14 11:10:04 -0500

    introduce fahrenheit to celsius function

--- exercises/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
+++ exercises/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
@@ -1,5 +1,12 @@
+function fahrenheit2Celsius() {
+  return 0;
+}
+
 describe('convert fahrenheit to celsius', () => {
-  it.only('32 should return 0');
+  it.only('32 should return 0', () => {
+    fahrenheit2Celsius(32).should.equal(0);
+  });
+  
   it.only('212 should return 100');
   it.only('-27 should return -32.77');
 });
