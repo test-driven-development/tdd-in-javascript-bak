8995d154abdcf2fcda23db4a989acbfdf1427bdc
wpannel2 <wpannel2@ford.com>
2019-11-18 16:05:30 -0500

    design stack

---
+++ exercises/03-stack/stack.spec.js
@@ -0,0 +1,13 @@
+describe.only('about stack', () => {
+  it('starts empty');
+  it('starts with stack size 0');
+  it('is not empty when pushed');
+  it('size is 1 when pushed to empty stack');
+  it('leaves stack empty when pushed and popped');
+  it('leaves stack size 0 when pushed and popped');
+  it('overflows');
+  it('under-flows');
+  it('pops the same one pushed');
+  it('pops the same two pushed');
+  it('accepts only positive capacity');
+});
