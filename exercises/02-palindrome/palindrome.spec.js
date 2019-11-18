7816b397e88260fe316f10b5f5b0d87761862668
wpannel2 <wpannel2@ford.com>
2019-11-18 12:03:39 -0500

    design palindrome specs

---
+++ exercises/02-palindrome/palindrome.spec.js
@@ -0,0 +1,11 @@
+describe.only('palindrome should ', () => {
+  it('return true if `mom`');
+  it('return false if `the`');
+  it('return true if `mom mom`');
+  it('return false if `the mom`');
+  it('return error if not a string');
+  it('return error if empty string');
+  it('return true if  `Mom`');
+});
+
+
