845b228db4d5e08bf93d5e6b9279e1ae8a794674
wpannel2 <wpannel2@ford.com>
2019-11-18 16:21:51 -0500

    refactor for legibility

--- exercises/03-stack/stack.spec.js
+++ exercises/03-stack/stack.spec.js
@@ -1,14 +1,12 @@
 let stack = {
-  isEmpty: () => {
-  return true;
-  }
+  isEmpty: () => true
 };
 
 describe.only('about stack', () => {
-
   it('starts empty', () => {
     stack.isEmpty().should.be.true();
   });
+
   it('starts with stack size 0');
   it('is not empty when pushed');
   it('size is 1 when pushed to empty stack');
