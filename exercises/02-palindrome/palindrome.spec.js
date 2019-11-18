d24359b5215e952f3bab5a18ef7e8ee23df93331
wpannel2 <wpannel2@ford.com>
2019-11-18 14:15:55 -0500

    improve the legibility of console output

--- exercises/02-palindrome/palindrome.spec.js
+++ exercises/02-palindrome/palindrome.spec.js
@@ -12,8 +12,11 @@ describe.only('palindrome should return', () => {
   it('true if `mom mom`', () => {
     isPalindrome('mom mom').should.be.true();
   });
-  
-  it('false if `the mom`');
+
+  it('false if `the mom`', () => {
+    isPalindrome('the mom').should.be.false();
+  });
+
   it('error if not a string');
   it('error if empty string');
   it('true if  `Mom`');
