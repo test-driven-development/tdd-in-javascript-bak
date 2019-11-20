7db688158ed94fe9c12a71100ba288a69e377e32
wpannel2 <wpannel2@ford.com>
2019-11-20 13:47:40 -0500

    introduce new test case and refactor push function

--- exercises/03-stack/stack.spec.js
+++ exercises/03-stack/stack.spec.js
@@ -1,15 +1,19 @@
 const stackFactory = () => {
   let stackSize = 0;
+  let queue;
   const capacity = 1;
   const isEmpty = () => stackSize === 0;
   const size = () => stackSize;
+  
   const pop = () => {
     if (stackSize === 0) throw new Error('Stack Underflow');
     stackSize--;
+    return queue;
   };
 
-  const push = () => {
+  const push = (element) => {
     if (capacity <= stackSize) throw new Error('Stack Overflow');
+    queue = element;
     stackSize++;
   };
 
@@ -65,7 +69,12 @@ describe.only('about stack', () => {
     (() => stack.pop()).should.throw('Stack Underflow');
   });
 
-  it('pops the same one pushed');
+  it('pops the same one pushed', () => {
+    const expected = {};
+    stack.push(expected);
+    stack.pop().should.equal(expected);
+  });
+
   it('pops the same two pushed');
   it('accepts only positive capacity');
 });
