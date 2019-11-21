f087f4450b7d5fb36eac3ed5e74f9302cb1b29c9
wpannel2 <wpannel2@ford.com>
2019-11-21 16:23:46 -0500

    introduce first test case

--- exercises/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
+++ exercises/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
@@ -1,5 +1,11 @@
+let fahrenheitToCelsius = () => {
+  return 0;
+};
+
 describe.only('fahrenheit to celsius should convert ', () => {
-  it('32F to 0C');
+  it('32F to 0C', () => {
+    fahrenheitToCelsius(32).should.equal(0);
+  });
   it('212F to 100C');
   it('-148F to -100C');
 });
