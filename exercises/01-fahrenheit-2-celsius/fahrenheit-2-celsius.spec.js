16bbf4bf3fbfc66bb833f7d4c147b4f3795f3e5f
wpannel2 <wpannel2@ford.com>
2019-11-22 08:24:42 -0500

    introduce remaining tests and apply 4 rules

--- exercises/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
+++ exercises/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
@@ -1,13 +1,17 @@
-let fahrenheitToCelsius = () => {
-  return 0;
-};
+let fahrenheitToCelsius = (fahrenheit) => (fahrenheit - 32) * (5 / 9);
 
-describe.only('fahrenheit to celsius should convert ', () => {
+describe('fahrenheit to celsius should convert ', () => {
   it('32F to 0C', () => {
     fahrenheitToCelsius(32).should.equal(0);
   });
-  it('212F to 100C');
-  it('-148F to -100C');
+
+  it('212F to 100C', () => {
+    fahrenheitToCelsius(212).should.equal(100);
+  });
+
+  it('-148F to -100C', () => {
+    fahrenheitToCelsius(-148).should.equal(-100);
+  });
 });
 
 
