9d85af6511e236b16e0685e366323db0a03ffdc7
wpannel2 <wpannel2@ford.com>
2019-11-22 08:58:14 -0500

    finalize design for palindrome

---
+++ exercises/02-palindrome/palindrome.spec.js
@@ -0,0 +1,11 @@
+describe.only(`a palindrome should return`, () => {
+  it(`true for input 'Anna'`);
+  it(`false for the input 'Frog' `);
+  it(`true for the phrase 'Able was I ere I saw Elba'`);
+  it(`false for the phrase 'Never odd or even'`);
+  it(`false for any number`);
+  it(`false for an array`);
+  it(`an error for a null input`);
+  it(`an error for an undefined input`);
+  it(`an error for an empty input string`);
+});
